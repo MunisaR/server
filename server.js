85c6e0e140ea080698600b4a161d4ccbf3948f16
MunisaR <munisrahmatullayeva04@gmail.com>
2023-01-30 21:09:35 +0500

    feat: deleted .env

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ app.post("/block_user", async function (req, res) {
   }
 });
 
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 app.listen(port, () => {
   console.log("server is running on port " + port);
